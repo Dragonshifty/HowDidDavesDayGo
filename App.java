31d1e81d809543c96065f696aacb4f878ffbf55a
James Fitt <91226933+Dragonshifty@users.noreply.github.com>
2023-03-16 11:41:12 +0000

    Add files via upload

--- App.java
+++ App.java
@@ -4,12 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         WorkingDay workingDay = new WorkingDay();
-        // for (int i = 0;  i < 100; i++){
         workingDay.staffArrive();
         workingDay.morningShift();
         workingDay.lunchShift();
         workingDay.afternoonShift();
-        // }
-        // workingDay.runTesting();
     }
 }
